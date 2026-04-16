eec8e4434a4599e6f4a579263bb3c95fed0dcabe
DonatoMorra <donatomorra90@gmail.com>
2026-04-16 15:09:49 +0200

    Update Utente.java

--- security/src/main/java/com/example/security/model/Utente.java
+++ security/src/main/java/com/example/security/model/Utente.java
@@ -1,10 +1,15 @@
 package com.example.security.model;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,4 +32,14 @@ public class Utente {
   private String username;
   private String password;
   private String ruolo; 
+
+
+
+    @ManyToOne
+    @JoinColumn(name = "manager_id")
+    private Utente manager; 
+
+    @OneToMany(mappedBy = "manager")
+    private List<Utente> team;
+
 }
